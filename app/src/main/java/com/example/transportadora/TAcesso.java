982e67ba37ffd3e21d3c5b736bd868649b0b1908
Marrowsed <marcus.ah@gmail.com>
2021-09-03 09:40:21 -0300

    New Buttons !
    Novos botões !

--- app/src/main/java/com/example/transportadora/TAcesso.java
+++ app/src/main/java/com/example/transportadora/TAcesso.java
@@ -6,7 +6,11 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 
 public class TAcesso extends AppCompatActivity {
@@ -18,9 +22,18 @@ public class TAcesso extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tacesso);
 
+        //Barra de cima
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //Botão de Ajuda
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(view -> {
+            Intent it = new Intent(TAcesso.this, Ajuda.class);
+            startActivity(it);
+        });
+
+        //Menu lateral
         drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
                 drawer,
@@ -32,6 +45,7 @@ public class TAcesso extends AppCompatActivity {
 
     }
 
+    //Chamada da função para abertura/fechamento do menu
     public void onBackPressed() {
         if (drawer.isDrawerOpen(GravityCompat.START)){
             drawer.closeDrawer(GravityCompat.START);
