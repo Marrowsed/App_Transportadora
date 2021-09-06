8b99ef5c45664797c22c03e31bf5bf78b11d9a6e
Marrowsed <marcus.ah@gmail.com>
2021-09-05 23:44:12 -0300

    Some adjustments !
    Alguns ajustes

--- app/src/main/java/com/example/transportadora/Ajuda.java
+++ app/src/main/java/com/example/transportadora/Ajuda.java
@@ -2,7 +2,10 @@ package com.example.transportadora;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class Ajuda extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class Ajuda extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ajuda);
+
+        //Home button - Botão Home
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(view -> {
+            Intent it = new Intent(Ajuda.this, TAcesso.class);
+            startActivity(it);
+        });
+
+
     }
 }
