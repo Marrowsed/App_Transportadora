72bb437b2852f497552d695a5725f0784e0a710c
Marrowsed <marcus.ah@gmail.com>
2021-09-05 23:46:18 -0300

    Login Screen !
    Tela de Login !

--- app/src/main/java/com/example/transportadora/TLogin.java
+++ app/src/main/java/com/example/transportadora/TLogin.java
@@ -3,8 +3,11 @@ package com.example.transportadora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class TLogin extends AppCompatActivity {
+
+    Button l1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
