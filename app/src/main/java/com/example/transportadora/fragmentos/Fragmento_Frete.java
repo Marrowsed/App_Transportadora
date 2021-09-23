5e2ee2dcf65e4ae6858c5047804d7f42d1a89109
Marrowsed <marcus.ah@gmail.com>
2021-09-22 22:26:01 -0300

    Clean code !
    Código limpo !

--- app/src/main/java/com/example/transportadora/fragmentos/Fragmento_Frete.java
+++ app/src/main/java/com/example/transportadora/fragmentos/Fragmento_Frete.java
@@ -14,7 +14,7 @@ import androidx.fragment.app.Fragment;
 import com.example.transportadora.R;
 import com.example.transportadora.mascara.CodeMask;
 
-public class Fragmento_Frete extends Fragment {
+public class Fragmento_Frete extends Fragment implements View.OnClickListener {
 
     EditText d1, d2, d3, peso, valor, pesoc, cepo, cepd;
     Button calcula;
@@ -39,27 +39,34 @@ public class Fragmento_Frete extends Fragment {
         cepo.addTextChangedListener(CodeMask.mask(cepo, CodeMask.FORMAT_CEP));
         cepd.addTextChangedListener(CodeMask.mask(cepd, CodeMask.FORMAT_CEP));
 
-        calcula.setOnClickListener(v -> {
-            Float dim1 = Float.valueOf(d1.getText().toString());
-            Float dim2 = Float.valueOf(d2.getText().toString());
-            Float dim3 = Float.valueOf(d3.getText().toString());
-            Float pesar = Float.valueOf(peso.getText().toString());
-            Float cubo = ((dim1 * dim2 * dim3)/6000);
+        calcula.setOnClickListener(this);
+        }
+
+
+    @Override
+    public void onClick(View view){
+        switch (view.getId()) {
+            case R.id.btn_calc:
+                Float dim1 = Float.valueOf(d1.getText().toString());
+                Float dim2 = Float.valueOf(d2.getText().toString());
+                Float dim3 = Float.valueOf(d3.getText().toString());
+                Float pesar = Float.valueOf(peso.getText().toString());
+                Float cubo = ((dim1 * dim2 * dim3)/6000);
                 if (dim1.equals("") || dim2.equals("") || dim3.equals("")){
                     Toast.makeText(getActivity(), "Valores Incompletos", Toast.LENGTH_SHORT).show();
                 } else
                 if (cubo <= 0){
                     Toast.makeText(getActivity(), "Valores Errados", Toast.LENGTH_SHORT).show();
                 }else
-                    if (cubo > pesar){
+                if (cubo > pesar){
                     valor.setText(("R$10,00"));
                     pesoc.setText(Float.toString(cubo));
                 } else {
                     valor.setText(("R$15,00"));
                     pesoc.setText(Float.toString(cubo));
                 }
-
-        });
+        }
+        }
     }
-}
+
 
