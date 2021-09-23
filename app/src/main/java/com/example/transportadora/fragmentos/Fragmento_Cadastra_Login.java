6ee82b6a7c3af52c1b6db7915800415c63e2bb9a
Marrowsed <marcus.ah@gmail.com>
2021-09-23 09:32:23 -0300

    Clean code !
    Código limpo !

--- app/src/main/java/com/example/transportadora/fragmentos/Fragmento_Cadastra_Login.java
+++ app/src/main/java/com/example/transportadora/fragmentos/Fragmento_Cadastra_Login.java
@@ -16,9 +16,9 @@ import com.example.transportadora.ManipulaDB;
 import com.example.transportadora.R;
 import com.example.transportadora.TLogin;
 
-public class Fragmento_Cadastra_Login extends Fragment {
+public class Fragmento_Cadastra_Login extends Fragment implements View.OnClickListener {
 
-    EditText user, pass;
+    EditText user, pass, confirmpass;
     Button finaliza;
     ManipulaDB bd;
 
@@ -36,23 +36,10 @@ public class Fragmento_Cadastra_Login extends Fragment {
         user = getView().findViewById(R.id.cad_user);
         pass = getView().findViewById(R.id.cad_pass);
         finaliza = getView().findViewById(R.id.btn_final);
+        confirmpass = getView().findViewById(R.id.cad_confirma);
         bd = new ManipulaDB(getActivity());
 
-        finaliza.setOnClickListener(v -> {
-           String usuario = user.getText().toString();
-           String senha = pass.getText().toString();
-           Boolean checa = bd.isUser(usuario);
-
-            if (usuario.equals("") || senha.equals("")) {
-                Toast.makeText(getActivity(), "Credenciais Inválidas", Toast.LENGTH_SHORT).show();
-            } else
-            if(checa == false){
-               cadastraUser(usuario, senha);
-               Intent it = new Intent(getActivity(), TLogin.class);
-               startActivity(it);
-            } else
-               Toast.makeText(getActivity(), "Usuário já existente !", Toast.LENGTH_SHORT).show();
-            });
+        finaliza.setOnClickListener(this);
         }
 
 
@@ -64,6 +51,15 @@ public class Fragmento_Cadastra_Login extends Fragment {
                 Toast.makeText(getActivity(), "Usuário já existente !", Toast.LENGTH_SHORT).show();
             }
         return true;
+    }
+
+    public Boolean confirmaPass(String senha, String confirma){
+        Boolean confirmar = senha.equals(confirma);
+        if (confirmar == true){
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public Boolean cadastraUser(String user, String pass) {
@@ -78,6 +74,30 @@ public class Fragmento_Cadastra_Login extends Fragment {
             }
         }
         return true;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_final:
+                String usuario = user.getText().toString();
+                String senha = pass.getText().toString();
+                String confirma = confirmpass.getText().toString();
+                Boolean checa = bd.isUser(usuario);
+
+                if (usuario.equals("") || senha.equals("") || confirma.equals("")) {
+                    Toast.makeText(getActivity(), "Credenciais Inválidas", Toast.LENGTH_SHORT).show();
+                } else if(checa == false) {
+                    if (confirmaPass(senha, confirma) == true) {
+                        cadastraUser(usuario, senha);
+                        Intent it = new Intent(getActivity(), TLogin.class);
+                        startActivity(it);
+                    } else
+                        Toast.makeText(getActivity(), "Senha errada !", Toast.LENGTH_SHORT).show();
+                }else
+                    Toast.makeText(getActivity(), "Usuário já existente !", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
 
