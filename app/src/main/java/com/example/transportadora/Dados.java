3470446227243069be5cf1844ad696fa61f9d747
Marrowsed <marcus.ah@gmail.com>
2021-10-05 17:42:04 -0300

    Data Class
    Classe de dados

---
+++ app/src/main/java/com/example/transportadora/Dados.java
@@ -0,0 +1,22 @@
+package com.example.transportadora;
+
+public class Dados {
+
+    static String login, CNPJ;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getCNPJ() {
+        return CNPJ;
+    }
+
+    public void setCNPJ(String CNPJ) {
+        this.CNPJ = CNPJ;
+    }
+}
