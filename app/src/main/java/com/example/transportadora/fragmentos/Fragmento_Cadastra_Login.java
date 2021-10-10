717620a1fd2bccf89b598f07cfefa1cdfb8db87e
Marrowsed <marcus.ah@gmail.com>
2021-10-10 13:18:47 -0300

    Layout Changes
    Mudanças no Layout

--- app/src/main/java/com/example/transportadora/fragmentos/Fragmento_Cadastra_Login.java
+++ app/src/main/java/com/example/transportadora/fragmentos/Fragmento_Cadastra_Login.java
@@ -1,31 +1,39 @@
 package com.example.transportadora.fragmentos;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.Handler;
+import android.text.InputType;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.transportadora.Dados;
 import com.example.transportadora.ManipulaDB;
 import com.example.transportadora.R;
-import com.example.transportadora.TLogin;
+import com.example.transportadora.TAcesso;
 
 import java.text.Normalizer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Fragmento_Cadastra_Login extends Fragment implements View.OnClickListener {
+public class Fragmento_Cadastra_Login extends Fragment implements View.OnClickListener, View.OnTouchListener {
 
     EditText user, pass, confirmpass;
+    TextView txt_pass;
     Button finaliza;
     ManipulaDB bd;
-    String empresa;
+    Dados data;
+    String CNPJ;
 
     @Nullable
     @Override
@@ -38,22 +46,38 @@ public class Fragmento_Cadastra_Login extends Fragment implements View.OnClickLi
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Bundle bundle = this.getArguments();
-        if (bundle != null) {
-            empresa = bundle.getString("CNPJ");
-            setEmpresa(empresa);
-        }
+        bd = new ManipulaDB(getActivity());
+        data = new Dados();
+        CNPJ = data.getCNPJ();
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        //Font
+        Typeface fonte = Typeface.createFromAsset(getActivity().getAssets(), "fonts/IBMPlexSans-Bold.ttf");
+
+        //View
         user = getView().findViewById(R.id.cad_user);
+        user.setTypeface(fonte);
         pass = getView().findViewById(R.id.cad_pass);
-        finaliza = getView().findViewById(R.id.btn_final);
+        pass.setTypeface(fonte);
         confirmpass = getView().findViewById(R.id.cad_confirma);
-        bd = new ManipulaDB(getActivity());
+        confirmpass.setTypeface(fonte);
+        txt_pass = getView().findViewById(R.id.txt_pass);
+        txt_pass.setText(
+                "Crie sua senha com 8 dígitos sendo:\n" +
+                        "1 Número \n" +
+                        "1 Letra Maiúscula \n" +
+                        "1 Letra Minúscula \n" +
+                        "1 Caractere Especial");
+        txt_pass.setTypeface(fonte);
+        finaliza = getView().findViewById(R.id.btn_final);
+
 
         finaliza.setOnClickListener(this);
+        pass.setOnClickListener(this);
+        pass.setOnTouchListener(this);
+        confirmpass.setOnTouchListener(this);
         }
 
 
@@ -99,7 +123,6 @@ public class Fragmento_Cadastra_Login extends Fragment implements View.OnClickLi
                 String senha = pass.getText().toString();
                 String confirma = confirmpass.getText().toString();
                 Boolean checa = bd.isUser(usuario);
-                String CNPJ = getEmpresa();
 
                 if (usuario.equals("") || senha.equals("") || confirma.equals("")) {
                     Toast.makeText(getActivity(), "Credenciais Inválidas", Toast.LENGTH_SHORT).show();
@@ -107,22 +130,27 @@ public class Fragmento_Cadastra_Login extends Fragment implements View.OnClickLi
                     validateUser(usuario);
                     if (validatePass(senha) && confirmaPass(senha, confirma) == true) {
                         cadastraUser(usuario, senha, CNPJ);
-                        Intent it = new Intent(getActivity(), TLogin.class);
+                        data.setLogin(usuario);
+                        Intent it = new Intent(getActivity(), TAcesso.class);
                         startActivity(it);
                     } else
                         Toast.makeText(getActivity(), "Senha errada !", Toast.LENGTH_SHORT).show();
                 }else
                     Toast.makeText(getActivity(), "Usuário já existente !", Toast.LENGTH_SHORT).show();
                 break;
+            case R.id.cad_pass:
+                Handler handler = new Handler();
+                txt_pass.setVisibility(View.VISIBLE);
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        if(txt_pass.getVisibility() == View.VISIBLE){
+                            txt_pass.setVisibility(View.GONE);
+                        }
+                    }
+                }, 5000);
+                break;
         }
-    }
-
-    public String getEmpresa (){
-        return empresa;
-    }
-
-    public void setEmpresa(String empresa){
-        this.empresa = empresa;
     }
 
     public static String validateUser (String user){
@@ -145,6 +173,35 @@ public class Fragmento_Cadastra_Login extends Fragment implements View.OnClickLi
             return false;
         }
         return isPass;
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        switch (view.getId()){
+            case R.id.cad_pass:
+                final int DRAWABLE_RIGHT = 2;
+                Typeface fonte_padrao = Typeface.createFromAsset(getActivity().getAssets(), "fonts/IBMPlexSans-Bold.ttf");
+                if (motionEvent.getRawX() >= (pass.getRight() - pass.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+                    pass.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                    pass.setTypeface(fonte_padrao);
+                    return true;
+                } else {
+                    pass.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                }
+                break;
+            case R.id.cad_confirma:
+                final int RIGHT = 2;
+                Typeface fonte = Typeface.createFromAsset(getActivity().getAssets(), "fonts/IBMPlexSans-Bold.ttf");
+                if (motionEvent.getRawX() >= (confirmpass.getRight() - confirmpass.getCompoundDrawables()[RIGHT].getBounds().width())) {
+                    confirmpass.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                    confirmpass.setTypeface(fonte);
+                    return true;
+                } else {
+                    confirmpass.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                }
+                break;
+        }
+        return false;
     }
 }
 
